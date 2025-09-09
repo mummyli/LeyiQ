574af4041f26c1af1c5cf660b92ac2fc26502183
l4j <mummy_li@163.com>
2025-09-09 22:20:34 +0800

    processor

---
+++ cmd/processor.py
@@ -0,0 +1,30 @@
+import argparse
+from core.rule_loader import load_rules_from_yaml
+from core.rule_registry import RuleRegistry
+from core.result_writer import ResultWriter
+from core.executors.batch_executor import BatchExecutor
+import pandas as pd
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rules", default="rules/sample_rules.yaml")
+    parser.add_argument("--csv", required=True, help="path to csv for dataset")
+    parser.add_argument("--dataset", required=True)
+    args = parser.parse_args()
+
+    rules = load_rules_from_yaml(args.rules)
+    
+    # register rules
+    registry = RuleRegistry()
+    for r in rules:
+        registry.register(r)
+        registry.publish(r.id)
+    
+    writer = ResultWriter()
+    be = BatchExecutor(registry, writer)
+    df = pd.read_csv(args.csv)
+    res = be.run_batch(args.dataset, df)
+    print("Results:", res)
+
+if __name__ == "__main__":
+    main()
