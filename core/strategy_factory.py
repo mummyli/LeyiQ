c9ed82a01b4a6d0e90fb3115841332168a563bcb
l4j <mummy_li@163.com>
2025-09-22 21:43:51 +0800

    add enum support

--- core/strategy_factory.py
+++ core/strategy_factory.py
@@ -6,6 +6,7 @@ from core.strategies.conditional_mandatory import ConditionalMandatoryStrategy
 from core.strategies.date_range import DateTimeRangeStrategy
 from core.strategies.str_length import StrLengthStrategy
 from core.strategies.whitespaces import WhitespaceStrategy
+from core.strategies.enum_value import EnumValueStrategy
 
 _STRATEGIES: Dict[str, Callable] = {
     "required": RequiredStrategy,
@@ -14,7 +15,8 @@ _STRATEGIES: Dict[str, Callable] = {
     "conditional_mandatory": ConditionalMandatoryStrategy,
     "datetime_range": DateTimeRangeStrategy,
     "str_length": StrLengthStrategy,
-    "whitespace": WhitespaceStrategy
+    "whitespace": WhitespaceStrategy,
+    "enum": EnumValueStrategy
 }
 
 def get_strategy(name: str):
